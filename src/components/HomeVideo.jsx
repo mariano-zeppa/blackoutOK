504d7544ff9d7af1358b7bef0ec4d0bd32b8825b
Mariano Zeppa <zeppamariano@gmail.com>
2021-03-06 19:47:34 +1300

    logo in ;)

--- src/components/HomeVideo.jsx
+++ src/components/HomeVideo.jsx
@@ -8,10 +8,10 @@ function HomeVideo () {
 <>
 
     <div className="relative flex flex-wrap justify-center h-screen overflow-hidden">
-      <div className="relative h-2/5 w-2/5 flex flex-wrap items-center h-screen overflow-hidden">
+      <div className="relative h-2/6 w-2/6 flex flex-wrap items-center h-screen overflow-hidden">
         <div className='absolute z-30  flex flex-wrap p-5 bottom-0 text-sm rounded-xl'>
       </div>
-       <img  className="relative z-30 p-5 text-2xl bg-indigo-100 bg-opacity-10 " src="blackoutlogo.png" alt=""/>
+       <img  className="relative z-30 p-5 text-2xl bg-indigo-300 bg-opacity-10 box-border border-4 rounded-full border-indigo-300 border-opacity-20 " src="blackoutlogoOK.png" alt=""/>
     </div>
     <video
       autoPlay
