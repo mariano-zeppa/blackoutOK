fda369a1cb388031a9a06b033bd87f274b76702c
Mariano Zeppa <zeppamariano@gmail.com>
2021-03-24 16:21:11 +1300

    added footer past events and events

--- src/components/ActualEvents.jsx
+++ src/components/ActualEvents.jsx
@@ -7,7 +7,13 @@ function ActualEvents () {
     
     <div className="bg-black flex items-center justify-center h-screen overflow-hidden text-white" id="nextevents">
       
-<div></div>
+    <div class="card">
+  <img src="blackoutevent.jpg" alt="" class="card__img"/>
+  <div class="card__body">
+    <h3 class="card__title">Sunset Edition</h3>
+    <p class="card__text">HELLO SUNSHINE ROOFTOP</p>
+  </div>
+</div>
 
 
     </div>
--- src/components/PastEvents.jsx
+++ src/components/PastEvents.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ScrollDown from './ScrollDown'
+import './PastEvents.css'
 function PastEvents () {
     
   return (
     <>
-<div className="bg-black flex items-center justify-center h-screen overflow-hidden text-white" id="pastevents">PastEvents</div>
+<div className="bg-black flex items-center justify-center h-screen overflow-hidden text-white" id="pastevents">
+  
+<div class="card">
+  <img src="blackoutevent.jpg" alt="" class="card__img"/>
+  <div class="card__body">
+    <h3 class="card__title">Sunset Edition</h3>
+    <p class="card__text">HELLO SUNSHINE ROOFTOP</p>
+  </div>
+</div>
+
+
+
+</div>
+
 
 </>
   )
