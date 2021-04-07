a4f929b1699b34e5e1512c50769c9445b9f9d2cc
Mariano Zeppa <zeppamariano@gmail.com>
2021-04-07 16:53:03 +1200

    updates on carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,23 +4,23 @@ import { Carousel } from "react-responsive-carousel";
 export default () => (
   <Carousel autoPlay>
     
-    <div className="mt-10">
+    <div className="mt-10 mb-10" id="gallery"> 
       <img className="" alt="" src="imageee.jpeg" />
       {/* <p className="legend">Legend 1</p> */}
     </div>
-    <div className="mt-10">
+    <div className="mt-10 mb-10">
       <img className="" alt="" src="30560.jpg" />
       {/* <p className="legend">Legend 1</p> */}
     </div>
-    <div className="mt-10">
+    <div className="mt-10 mb-10">
       <img className="" alt="" src="81834.jpg" />
       {/* <p className="legend">Legend 1</p> */}
     </div>
-    <div className="mt-10">
+    <div className="mt-10 mb-10">
       <img className="" alt="" src="imageee.jpeg" />
       {/* <p className="legend">Legend 1</p> */}
     </div>
-    <div className="mt-10">
+    <div className="mt-10 mb-10">
       <img className="" alt="" src="imageee.jpeg" />
       {/* <p className="legend">Legend 1</p> */}
     </div>
